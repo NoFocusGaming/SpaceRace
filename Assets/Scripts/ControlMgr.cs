89ca3133381d7df585b9196a94ea9b1c8ad02e06
Salem Bitney <sbitney@nevada.unr.edu>
2023-04-25 23:22:19 -0700

    added UI functionality and got rid of compiler/runtime errors
    movement still not working, sorry :/

--- Assets/Scripts/ControlMgr.cs
+++ Assets/Scripts/ControlMgr.cs
@@ -11,46 +11,32 @@ public class ControlMgr : MonoBehaviour
         inst = this;
     }
 
-    //public Cart playerOne;
+    public Cart playerOne;
     float deltaHeading;
     public float deltaSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        //deltaHeading = playerOne.turnRate;
+        deltaHeading = playerOne.turnRate;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Keyboard.current.escapeKey.wasPressedThisFrame)
+        if (Input.GetKeyDown(KeyCode.Escape))
             Application.Quit();
 
-        /*
-        if(Input.GetKey(KeyCode.UpArrow))
+
+        if (Input.GetKey(KeyCode.UpArrow))
             playerOne.desiredSpeed += deltaSpeed;
         else
             playerOne.desiredSpeed -= deltaSpeed;
        playerOne.desiredSpeed = Utils.Clamp(playerOne.desiredSpeed, playerOne.minSpeed, playerOne.maxSpeed);
 
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
             playerOne.heading -= deltaHeading;
         if(Input.GetKey(KeyCode.RightArrow))
             playerOne.heading += deltaHeading;
-        */
     }
-
-    /*void OnTurnRight()
-    {
-        playerOne.heading += deltaHeading;
-    }
-    void OnTurnLeft()
-    {
-        playerOne.heading -= deltaHeading;
-    }
-    void OnAccelerate()
-    {
-        playerOne.desiredSpeed += deltaSpeed;
-    }*/
 }
--- Assets/Scripts/UIMgr.cs
+++ Assets/Scripts/UIMgr.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UIMgr : MonoBehaviour
 {
+    // text setting for placement
+    public TextMeshProUGUI placeText;
+    public TextMeshProUGUI lapText;
+
+    public int lap;
+    public int maxLap;
+    public int place;
+
+    // image setting for held item
+
+    public Image currItem;
+
+    public List<Sprite> items;
+    public int spriteIndex;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        lap = 0;
+        place = 1;
+
+        spriteIndex = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (place)
+        {
+            case 1:
+                placeText.SetText(place + "st!");
+                break;
+            case 2:
+                placeText.SetText(place + "nd!");
+                break;
+            case 3:
+                placeText.SetText(place + "rd!");
+                break;
+            default:
+                placeText.SetText(place + "th!");
+                break;
+        }
+        lapText.SetText(lap + "/" + maxLap);
+
+        currItem.sprite = items[spriteIndex];
     }
 }
